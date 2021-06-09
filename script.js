bb375ce68041aa9a8b13c873fd1f0a30b58c5f9c
Sebasitan Manco Gomez <sebasmg1393@gmail.com>
2021-06-09 14:45:51 -0500

    render workoutk ok

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -22,7 +21,15 @@ class Workout{
     this.coords = coords; // [lat, lng]
     this.distance = distance; // in Km
     this.duration = duration; // in min
+    
 
+  }
+
+  _setDescription(){
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
   }
 }
 
@@ -33,6 +40,7 @@ class Running extends Workout {
     super(coords, distance, duration);
     this.cadence = cadence;
     this.calcPace();
+    this._setDescription();
   }
 
   calcPace(){
@@ -49,6 +57,7 @@ class Cycling extends Workout {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
     this.calcSpeed();
+    this._setDescription();
   }
 
   calcSpeed(){
@@ -92,6 +101,7 @@ class App {
   }
 
   _loadMap(position) {
+
     // console.log(position);
     const { latitude, longitude } = position.coords;
     console.log(`https://www.google.com/maps/@${latitude}{longitude}`);
@@ -178,10 +188,12 @@ class App {
     console.log(workout);
 
     //Render workout on map marker
-    this.renderWorkoutMarker(workout);
+    this._renderWorkoutMarker(workout);
+
+    //Render workout on list.
+    this._renderWorkkout(workout)
 
     //Hide from + clear input fields
-
     //console.log(this);
     //Clear input fields
     inputDistance.value =
@@ -193,7 +205,7 @@ class App {
     
   }
 
-  renderWorkoutMarker(workout){
+  _renderWorkoutMarker(workout){
     //Display marker
     console.log('Hola entrenaste.');
     //Esto crea un marcador en el mapa
@@ -210,6 +222,61 @@ class App {
       )
       .setPopupContent('workout')
       .openPopup();
+  }
+
+  _renderWorkkout(workout){
+    //Dom manpulacion, crearemos un markout y lo insertaremos en el DOM.
+
+    let html = `
+      <li class="workout workout--${workout.type}" data-id="${workout.id}">
+        <h2 class="workout__title">${workout.description}</h2>
+        <div class="workout__details">
+          <span class="workout__icon">${workout.type === 'runniing' ? '🏃‍♂️' : '🚴‍♀️'}</span>
+          <span class="workout__value">${workout.distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+        </div>
+    `;
+
+    if(workout.type === 'running'){
+      html += `
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.pace.toFixed(1)}</span>
+          <span class="workout__unit">min/km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">🦶🏼</span>
+          <span class="workout__value">${workout.cadence}</span>
+          <span class="workout__unit">spm</span>
+        </div>
+      </li>
+      `;
+
+      if(workout.type === 'cycling'){
+        html += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>
+        
+        `;
+      }
+    }
+
+    form.insertAdjacentHTML('afterend',html);
+
   }
 
 
